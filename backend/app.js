fd63c7811ee322f3311e6998844a2becab5b82de
Sudhamoy Dey <1928325@kiit.ac.in>
2024-09-18 20:35:05 +0530

    Database connected and secret keys gets in the hidden part

--- backend/app.js
+++ backend/app.js
@@ -1,6 +1,7 @@
 import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
+import { dbConnection } from "./database/dbConnection.js";
 
 const app=express()
 dotenv.config({ path:"./config/config.env"})
@@ -13,5 +14,7 @@ app.use(cors({
 
 app.use(express.json())
 app.use(express.urlencoded({extended:true}))
+
+dbConnection()
 
 export default app
---
+++ backend/database/dbConnection.js
@@ -0,0 +1,11 @@
+import mongoose from "mongoose"
+
+export const dbConnection=()=>{
+  mongoose.connect(process.env.MONGO_URI,{
+    dbName:" tabletrek"
+  }).then(()=>{
+    console.log("Connected to MONGODB database successfully")
+  }).catch(err =>{
+    console.log(err)
+  })
+}
